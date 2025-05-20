57e2dfb4391f997263a473fe5a10f51e2738d58a
prathyusha-9896 <jfsjd.mum.prathyusha@gmail.com>
2025-05-20 17:39:52 +0530

    gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Header } from '@/_components'
 import './globals.css'
 import Footer from '@/_components/footer/Footer'
+import Script from 'next/script'
+import Link from 'next/link'
 
 export default function RootLayout({
   children,
@@ -15,21 +17,31 @@ export default function RootLayout({
         <link rel="icon" href="/svgfavicon.svg" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
+        <Link
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
           rel="stylesheet"
         />
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M2HQ4L2K');`}
+        </Script>
+        {/* End Google Tag Manager */}
+
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
-        {/*<noscript>
+        <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MTWQZBK"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M2HQ4L2K"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
-        </noscript>*/}
+        </noscript>
 
         <div className="flex flex-col flex-1 relative">
           <Header />

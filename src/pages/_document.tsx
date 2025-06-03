cd3581fdc01f2d9cfcfb1115e303d5723bac8b06
prathyusha-9896 <jfsjd.mum.prathyusha@gmail.com>
2025-06-03 11:13:07 +0530

    GTM 6

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,7 @@ export default function Document() {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MTWQZBK');
+              })(window,document,'script','dataLayer','GTM-M2HQ4L2K');
             `,
           }}
         />
@@ -28,7 +28,7 @@ export default function Document() {
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MTWQZBK"
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2HQ4L2K"
                       height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
           }}

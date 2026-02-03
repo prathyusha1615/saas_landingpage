aa7c98579edb064fb06fea294f4e62b21da1ff40
suraj-kumar89 <suraj@bluvodigital.com>
2026-02-03 21:38:58 +0530

    Remove no index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,12 @@ import './globals.css'
 import Footer from '@/_components/footer/Footer'
 import WhatsAppButton from '@/_components/WhatsApp'
 
-
+export const metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -13,8 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Preconnect for Google Fonts */}
-          {/* Favicon */}
         <link rel="icon" href="/svgfavicon.svg" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -22,19 +25,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
           rel="stylesheet"
         />
-        {/* Google Tag Manager */}
-        <script
-          id="gtm-script"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M2HQ4L2K');
-          `,
-          }}
-        />
+        {/* GTM script stays */}
       </head>
       <body>
         <div className="flex flex-col flex-1 relative">
@@ -43,15 +34,6 @@ export default function RootLayout({
           <Footer/>
         </div>
         <WhatsAppButton/>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M2HQ4L2K"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
       </body>
     </html>
   )
